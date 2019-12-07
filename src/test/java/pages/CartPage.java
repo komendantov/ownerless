1edc3f5b961160c0e8e48fa92c1a78a64f86d1c5
Anatoliy Komendantov <anatoliy.komendantov@sony.com>
2019-12-07 14:22:49 +0400

    cartRemoveAll method stabilized

--- src/test/java/pages/CartPage.java
+++ src/test/java/pages/CartPage.java
@@ -9,12 +9,18 @@ import java.util.function.Function;
 public class CartPage extends PageBase {
 
     private static final String EMPTY_CART_MESSAGE_LOCATOR = "//html//p[1]//em";
-    private static final String CART_REMOVE_PRODUCT_LOCATOR = "[name=remove_cart_item]";
+    private static final String CART_REMOVE_PRODUCT_LOCATOR = "//button[@name='remove_cart_item']";
     private static final String CART_FORM_LOCATOR = "[name =cart_form]";
 
-    public void cartRemoveAll(){
-        while (driver.findElements(By.xpath(EMPTY_CART_MESSAGE_LOCATOR)).size() != 1){
-            driver.findElement(By.cssSelector(CART_REMOVE_PRODUCT_LOCATOR)).click();
+    public CartPage(EventFiringWebDriver driver) {
+        super(driver);
+    }
+
+    public void cartRemoveAll() {
+        while (driver.findElements(By.xpath(EMPTY_CART_MESSAGE_LOCATOR)).size() != 1) {
+            wait.until((Function<WebDriver, Object>) driver ->
+                    driver.findElement(By.xpath(CART_REMOVE_PRODUCT_LOCATOR)).isDisplayed());
+            driver.findElement(By.xpath(CART_REMOVE_PRODUCT_LOCATOR)).click();
             Integer skuLines = driver.findElements(By.cssSelector(CART_FORM_LOCATOR)).size();
             wait.until((Function<WebDriver, Object>) driver -> driver
                     .findElements(By.cssSelector(CART_FORM_LOCATOR)).size() != skuLines ||
@@ -22,11 +28,7 @@ public class CartPage extends PageBase {
         }
     }
 
-    public boolean cartIsEmpty (){
+    public boolean cartIsEmpty() {
         return driver.findElement(By.xpath(EMPTY_CART_MESSAGE_LOCATOR)).isDisplayed();
-    }
-
-    public CartPage(EventFiringWebDriver driver) {
-        super(driver);
     }
 }
